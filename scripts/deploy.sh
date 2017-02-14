ece4e3ba2b6557b5912d5e1710999a2db218de74
yutopp <yutopp@gmail.com>
2017-02-14 18:55:41 +0900

    add ci(build and deploy)

---
+++ scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -ex
+
+GIT_USER_NAME="rill-docs-ci"
+GIT_USER_EMAIL="yutopp+rill-docs-ci@gmail.com"
+
+PROJECT_USERNAME="yutopp"
+PROJECT_REPONAME="rill-docs"
+GIT_REPO="git@github.com:${PROJECT_USERNAME}/${PROJECT_REPONAME}.git"
+
+WORK_DIR="_gh_pages"
+if [ "${WORK_DIR}" == "" ]; then exit 1; fi
+
+# make dir for gh-pages and clean files
+if [ ! -e ${WORK_DIR} ]; then
+    git clone -b gh-pages "${GIT_REPO}" ${WORK_DIR}
+fi
+rm -rf ${WORK_DIR}/*
+
+# copy files which are generated
+cp -r book/* ${WORK_DIR}/.
+
+# push
+cd ${WORK_DIR}
+git config user.name "${GIT_USER_NAME}"
+git config user.email "${GIT_USER_EMAIL}"
+git add --all
+git commit --allow-empty -m 'update by rill-docs-ci-deploy'
+git push -f origin gh-pages
